abb2a5710759b94ad30b26d1382cfcd18a07d531
Shohruxshoh <shohhhhhhhhhhhhhhhhhhhh@gmail.com>
2021-03-19 15:05:33 +0500

    yangi daploy1

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1b!%@l%5q_eu3pjsxja6#9n*3qdn(6h(ws1$wzh40v#ak8p%9a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['deltaportfolio1.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['deltaportfolio2.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
